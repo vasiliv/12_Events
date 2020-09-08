be2d88ba96defc5db5b1632b89026239ee076bff
grandiosa iva <ggo24@yahoo.com>
2020-09-08 10:42:52 +0400

    1

---
+++ 001_events/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_events
+{
+    public delegate void MyDelegate();
+    class MyClass
+    {
+        public event MyDelegate MyEvent;
+        public void InvokeEvent()
+        {
+            MyEvent.Invoke();
+        }
+    }
+    class Program
+    {
+        private static void EventHandler1()
+        {
+            Console.WriteLine("handler1");
+        }
+        private static void EventHandler2()
+        {
+            Console.WriteLine("handler2");
+        }
+        static void Main(string[] args)
+        {
+            MyClass instance = new MyClass();
+            instance.MyEvent += new MyDelegate(EventHandler1);
+            instance.MyEvent += EventHandler2; // no brackets
+
+            instance.InvokeEvent();
+            Console.ReadLine();
+        }
+    }
+}
