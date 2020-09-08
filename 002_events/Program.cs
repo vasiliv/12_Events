c76f42577f9c0ca10976d6b1ebc03488c7dda357
grandiosa iva <ggo24@yahoo.com>
2020-09-08 11:26:29 +0400

    2

---
+++ 002_events/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_events
+{
+    public delegate void MyDelegate();
+    public class MyClass
+    {
+        MyDelegate myEvent = null;
+        public event MyDelegate MyEvent
+        {
+            add { myEvent += value; } // myEvent didi asoti mecera. error cs0079!!!
+            remove { myEvent -= value; }
+        }
+        public void InvokeEvent()
+        {
+            myEvent.Invoke();
+        }
+    }
+    class Program
+    {
+        private static void handler1()
+        {
+            Console.WriteLine("handler1");
+        }
+        private static void handler2()
+        {
+            Console.WriteLine("handler2");
+        }
+        static void Main(string[] args)
+        {
+            MyClass instance = new MyClass();
+            instance.MyEvent += handler1;
+            instance.MyEvent += handler2;
+
+            instance.InvokeEvent();
+            Console.ReadLine();
+        }
+    }
+}
